1bbfa27f3b0823e22bc0a8bf01e32d27e980ee8c
DuaKingKong <crimson_sabre@hotmail.com>
2019-04-09 05:21:33 +0800

    Updates

---
+++ precache-manifest.381948a2982e5b0b587e81784d843073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab8a5f75dd36d5a32dc",
+    "url": "/simple-react-app/static/css/main.19ea24de.chunk.css"
+  },
+  {
+    "revision": "9ab8a5f75dd36d5a32dc",
+    "url": "/simple-react-app/static/js/main.584065cd.chunk.js"
+  },
+  {
+    "revision": "6fd64f1acbc6de00f5ea",
+    "url": "/simple-react-app/static/js/runtime~main.d5660f83.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/simple-react-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ffcebfdac2270310a7f8f532afa3305b",
+    "url": "/simple-react-app/index.html"
+  }
+];
